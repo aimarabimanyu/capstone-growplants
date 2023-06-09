8cb01de83e12d62d515441d8aaa27c7180897cfc
Rafli <ahmad.farizqi23@gmail.com>
2023-06-09 09:19:26 +0700

    Create historyController, model, & update route

--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const requireAuth = require("../middleware/requireAuth");
 const userController = require("../controller/userController");
+const historyController = require("../controller/historyController");
 
 
 router.use(requireAuth);
@@ -10,5 +11,7 @@ router.get("/profile", userController.getUser);
 // profile image route
 router.post("/profile/images", userController.updateUser);
 
+// history route
+router.get("/history", historyController.getHistory);
 
 module.exports = router;
